68d7d3619af97bf807264c1cf4bdcf13e3c841c9
Ilya Ponomarev <iponom0420@gmail.com>
2022-12-04 19:25:01 +0300

    day 4

---
+++ src/day4/Part2.java
@@ -0,0 +1,28 @@
+package day4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Part2 {
+    final static String INPUT_PATH = "src/day4/input";
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(INPUT_PATH));
+        int count = 0;
+        String s;
+        while ((s = in.readLine()) != null) {
+            String[] assignments = s.split(",");
+            String[] a1 = assignments[0].split("-");
+            String[] a2 = assignments[1].split("-");
+            int min1 = Integer.parseInt(a1[0]);
+            int max1 = Integer.parseInt(a1[1]);
+            int min2 = Integer.parseInt(a2[0]);
+            int max2 = Integer.parseInt(a2[1]);
+            if (max1 >= min2 && min1 <= max2) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
